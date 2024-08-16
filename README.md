52377508b8d123ff57860b831ec5c76e43da8893
Santosh <santmartian@gmail.com>
2024-08-16 14:40:53 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MyWeather
+
+A Simple Animated Weather app built in Flutter.
+
+~~apikey to be dropped as a string in lib/secrets.dart~~
---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,102 @@
+import 'package:MyWeather/services/weather_service.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import '../models/weather_model.dart';
+import 'package:MyWeather/secrets.dart';
+
+class MyWidget extends StatefulWidget{
+  const MyWidget({super.key});
+
+  State <MyWidget>createState()=>_MyWidgetState();
+  
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class _MyWidgetState extends State<MyWidget>{
+  String timeOfDay="";
+  DateTime now = DateTime.now();
+
+  final _weatherService=WeatherService(apiKey);
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName=await _weatherService.getCurrentCity();
+
+    try{
+      final weather=await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather=weather;
+      });
+    }
+
+    catch(e){
+      print(e); 
+    }
+  }
+
+  String getWeatherAnimation(String? mainCondition){
+    if (mainCondition==null) return "assets/${timeOfDay}sunny.json";
+    if (now.hour>=18) timeOfDay="night_";
+    switch (mainCondition.toLowerCase()){
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return "assets/${timeOfDay}cloudy.json";
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return "assets/${timeOfDay}rainy.json";
+      case 'thunderstorm':
+        return "assets/${timeOfDay}thunderstorm.json";
+      case 'clear':
+        return "assets/${timeOfDay}sunny.json";
+      default:
+        return "assets/${timeOfDay}sunny.json";
+    }
+
+  }
+
+
+  @override
+  void initState(){
+    super.initState();
+
+    _fetchWeather();
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: Colors.grey[900] ,
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+   
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.my_location,color: Colors.white,),
+                  SizedBox(width: 10,),
+                  Text(_weather?.cityName ?? "locating city",style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 32.0,color: Colors.white),),
+                ],
+              ),
+            ),
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+            Text(_weather?.mainCondition ?? "",style: const TextStyle(fontWeight: FontWeight.bold,fontSize: Checkbox.width,color: Colors.white),),
+            Text('${_weather?.temperature.round()}°C',style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 48,color: Colors.white),),
+          ],
+        ),
+      ),
+    );
+  }
+}
